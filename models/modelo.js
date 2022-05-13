24584c0c3aa6b98d9b0830e9df5e47743a17d756
Nestor <nestorlbp@gmail.com>
2022-05-13 03:04:40 -0500

    Agregada la ruta agregar modelo

--- models/modelo.js
+++ models/modelo.js
@@ -3,12 +3,21 @@ const mongoose = require('../db/database')
 const Schema = mongoose.Schema
 
 const ModeloSchema = new Schema({
+    modeloId: Number,
     nombre: String,
     apellido: String,
+    cedula: String,
+    alias: String,
+    sexo: String,
+    fechaNacimiento: String,
+    ciudad: String,
+    status: String,
     email: String,
-    telefono: String,
-    tipoUsuario: String,
-    contrasena: String
+    facebook: String,
+    twitter: String,
+    instagram: String,
+    youtube: String,
+    pornhub: String,
 })
 
 const Modelo = mongoose.model('modelos', ModeloSchema)
--- routes/rutas.js
+++ routes/rutas.js
@@ -1,121 +1,28 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
-const Usuario = require("../models/usuario");
 const Modelo = require("../models/modelo");
 
 
 const rutas = express.Router();
 
-let perfilDB = [
-  {
 
-  },
-];
-
-
-rutas.get("/usuarios", async (req, res) => {
-  let usuarios = await Usuario.find();
-  res.json(usuarios);
-});
-
-rutas.get("/usuario/:id", async (req, res) => {
-  let usuarioId = await Usuario.findById(req.params.id);
-  res.json(usuarioId);
-});
-
-rutas.post("/verificarContrasena/:id", async (req, res) => {
-  let usuarioId = await Usuario.findById(req.params.id);
-
-  let validarPassword = await bcrypt.compare(req.body.contrasenaActual, usuarioId.contrasena);
-
-  if (!validarPassword) {
-    return res.json({
-      mensaje: "CONTRASEÑA ACTUAL INVALIDA",
-    });
-  }else {
-    return res.json({
-      mensaje: "CORRECTA",
-    });
-  }
-});
-
-rutas.put("/actualizarContrasena/:id", async (req, res) => {
-
-  let salt = await bcrypt.genSalt(12);
-  let password = await bcrypt.hash(req.body.contrasenaNueva, salt);
-
-  const usuario = await Usuario.updateMany(
-    { _id: req.params.id } ,
-    { $set: { contrasena: password} }
- )
-  res.json(usuario);
-});
-
-rutas.put("/editarUsuario/:id", async (req, res) => {
-  const usuario = await Usuario.findById(req.params.id);
-
-  usuario.nombre = req.body.nombre;
-  usuario.apellido = req.body.apellido;
-  usuario.email = req.body.email;
-  usuario.telefono = req.body.telefono;
-  usuario.tipoUsuario = req.body.tipoUsuario;
-
-  await usuario.save();
-
-  res.json({ mensaje: "Usuario actualizado" });
-});
-
-rutas.get("/usuarioEmail/:email", async (req, res) => {
-  let usuarioId = await Usuario.findOne({ email: req.params.email });
-  res.json(usuarioId);
-});
-
-rutas.post("/buscarUsuario", async (req, res) => {
-  let buscar = req.body.buscar;
-  let usuarios = await Usuario.find({
-    $or: [{ nombre: { $regex: buscar } }, { apellido: { $regex: buscar } }, { email: { $regex: buscar } }, { tipoUsuario: { $regex: buscar } }, { telefono: { $regex: buscar } }],
-  });
-  res.json(usuarios);
-});
-
-rutas.post("/agregarUsuario", async (req, res) => {
+rutas.post("/agregarModelo", async (req, res) => {
   let datos = req.body;
-
-  let salt = await bcrypt.genSalt(12);
-  let password = await bcrypt.hash(datos.contrasena, salt);
-
+  let maxId = await Modelo.findOne().sort({modeloId:-1}).limit(1);
+  let id
+  if (!maxId){
+    id = 1
+  }else{
+    id = parseInt(maxId.modeloId) + 1;
+  }
   datos = {
     ...datos,
-    contrasena: password,
+    modeloId: id,
   };
-
-  let usuario = await new Usuario(datos);
-  await usuario.save();
-  res.json({ mensaje: "Usuario agregado" });
+  let modelo = await new Modelo(datos);
+  await modelo.save();
+  res.json({ mensaje: "Modelo agregado", MaxId: id});
 });
-
-rutas.put("/editarUsuario/:id", async (req, res) => {
-  const usuario = await Usuario.findById(req.params.id);
-
-  usuario.nombre = req.body.nombre;
-  usuario.apellido = req.body.apellido;
-  usuario.email = req.body.email;
-  usuario.telefono = req.body.telefono;
-  usuario.tipoUsuario = req.body.tipoUsuario;
-
-  await usuario.save();
-
-  res.json({ mensaje: "Usuario actualizado" });
-});
-
-rutas.delete("/eliminarUsuario/:id", async (req, res) => {
-  let usuario = await Usuario.findById(req.params.id);
-
-  await usuario.deleteOne();
-
-  res.json({ mensaje: "Usuario eliminado" });
-});
-
 
 
 rutas.get("/modelos", async (req, res) => {
@@ -123,10 +30,5 @@ rutas.get("/modelos", async (req, res) => {
   res.json(modelos);
 });
 
-
-
-rutas.get("/perfil", (req, res) => {
-  res.json(perfilDB);
-});
 
 module.exports = rutas;
